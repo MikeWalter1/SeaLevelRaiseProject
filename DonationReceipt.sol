0489e2fb4955b5e52daa49401f7b7c0359c410af
Mike Walter <32678311+MikeWalter1@users.noreply.github.com>
2024-04-28 17:30:37 +0200

    Refactor createDonationReceipt function to use external visibility and remove unused struct

--- DonationReceipt.sol
+++ DonationReceipt.sol
@@ -4,29 +4,24 @@ pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract DonationNFT is ERC721URIStorage {
-    struct Donation {
-        address donor;
-        uint amount;
-        uint timestamp;
-    }
     uint private nextTokenId;
     address payable public dao;
-    mapping(address => Donation[]) public donations;
     mapping(bytes32 => bool) public nftsCreated;
 
     constructor(address _dao) ERC721("SLR-DonationReceipt", "SLR-DR") {
         dao = payable(_dao);
+        //dao = payable(address(DAO_SLR));
         nextTokenId = 0;
     }
 
     // Function to mint NFTs for donors if a project receives enough votes
-    function createDonationReceipt(address _recipient, uint _projectId, uint _donationAmount, uint _timestamp) public {
+    function createDonationReceipt(address _recipient, uint _projectId, uint _donationAmount, uint _timestamp) external {
         require(msg.sender == dao, "Only DAO_SLR contract can access this function");
-
         
         bytes32 nftHash = hash(Strings.toHexString(uint256(uint160(_recipient)), 20), Strings.toString(_projectId), _donationAmount, _timestamp);
         require(!nftsCreated[nftHash], "NFT already created");
         nftsCreated[nftHash] = true;
+        
         //this has to happen somewhere else
         //require(_projectId < projectCount, "Invalid project ID");
         //require(projects[_projectId].currentFunding() >= projects[_projectId].fundingGoal(), "Project not funded yet");
@@ -43,11 +38,10 @@ contract DonationNFT is ERC721URIStorage {
 
         // Increment the next token ID
         nextTokenId += 1;
-        donations[_recipient].push(Donation(_recipient, _donationAmount, _timestamp));
     }
 
     function hash(string memory _recipient, string memory _projectId, uint _donationAmount, uint _timestamp) private pure returns(bytes32){
         string memory nftMetaHash = string(abi.encodePacked(_recipient, _projectId, _donationAmount, _timestamp));
-        return sha256(bytes(nftMetaHash));
+        return sha256(bytes(nftMetaHash)); //instead of sha256 use keccak256?
     }
 }
