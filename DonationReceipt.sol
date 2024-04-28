7569c2da61033b1e85240c83e79fb4f5c571c6a0
Mike Walter <32678311+MikeWalter1@users.noreply.github.com>
2024-04-28 17:08:32 +0200

    Refactor to inherit from ERC721URIStorage and add createDonationReceipt function

--- DonationReceipt.sol
+++ DonationReceipt.sol
@@ -1,22 +1,53 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-contract DonationNFT {
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract DonationNFT is ERC721URIStorage {
     struct Donation {
         address donor;
         uint amount;
         uint timestamp;
     }
-
+    uint private nextTokenId;
+    address payable public dao;
     mapping(address => Donation[]) public donations;
+    mapping(bytes32 => bool) public nftsCreated;
 
-    // Function to mint NFT for a donation
-    function mintNFT(address _recipient, uint _amount, uint _timestamp) public {
-        // Mint NFT with donation information
-        // Code for minting NFT goes here
-        
-        donations[_recipient].push(Donation(_recipient, _amount, _timestamp));
+    constructor(address _dao) ERC721("SLR-DonationReceipt", "SLR-DR") {
+        dao = payable(_dao);
+        nextTokenId = 0;
     }
 
-    // Other functions for managing NFTs, such as transferring ownership, viewing donation history, etc.
+    // Function to mint NFTs for donors if a project receives enough votes
+    function createDonationReceipt(address _recipient, uint _projectId, uint _donationAmount, uint _timestamp) public {
+        require(msg.sender == dao, "Only DAO_SLR contract can access this function");
+
+        
+        bytes32 nftHash = hash(Strings.toHexString(uint256(uint160(_recipient)), 20), Strings.toString(_projectId), _donationAmount, _timestamp);
+        require(!nftsCreated[nftHash], "NFT already created");
+        nftsCreated[nftHash] = true;
+        //this has to happen somewhere else
+        //require(_projectId < projectCount, "Invalid project ID");
+        //require(projects[_projectId].currentFunding() >= projects[_projectId].fundingGoal(), "Project not funded yet");
+        //require(!hasReceivedNFT[msg.sender], "NFT already received");
+
+        // Mint the NFT
+        _safeMint(_recipient, nextTokenId);
+
+        // Create the token URI with the donation details
+        string memory tokenURI = 
+        string(abi.encodePacked("data:application/json,{\"Project Title\":\"Donation NFT\",\"description\":\" + projects[_projectId].projectDescription() + ", _donationAmount, " ETH to the contract.\"}"));
+
+        _setTokenURI(nextTokenId, tokenURI);
+
+        // Increment the next token ID
+        nextTokenId += 1;
+        donations[_recipient].push(Donation(_recipient, _donationAmount, _timestamp));
+    }
+
+    function hash(string memory _recipient, string memory _projectId, uint _donationAmount, uint _timestamp) private pure returns(bytes32){
+        string memory nftMetaHash = string(abi.encodePacked(_recipient, _projectId, _donationAmount, _timestamp));
+        return sha256(bytes(nftMetaHash));
+    }
 }
