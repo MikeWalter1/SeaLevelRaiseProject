8c2cb4dfa8f02c4faf7b75a38240417e662033e8
Mike Walter <32678311+MikeWalter1@users.noreply.github.com>
2024-05-01 18:15:15 +0200

    Add addDonor and withdrawFunds functions to DonorManager contract

--- contracts/DonorManager.sol
+++ contracts/DonorManager.sol
@@ -9,6 +9,10 @@ struct Donor{
 contract DonorManager {
     mapping(address => Donor) public donors;
 
+    function addDonor(address _donor) public {
+        donors[_donor].donorAddress = _donor;
+    }
+
     function getDonorTokenBalance() public view returns(uint) {
     return donors[msg.sender].votingTokens;
     } 
@@ -21,4 +25,11 @@ contract DonorManager {
         require(donors[msg.sender].votingTokens >= _tokens, "Insufficient voting tokens");
         donors[msg.sender].votingTokens -= _tokens;
     }
+
+    function withdrawFunds(uint _amount) public {
+        require(donors[msg.sender].votingTokens >= _amount, "Insufficient funds");
+        donors[msg.sender].votingTokens -= _amount;
+        payable(msg.sender).transfer(_amount);
+    }
+
 }
